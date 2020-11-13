9b4361e64fe06be0350104010dc4f54b4632beb1
alejandro motta <alejandromotta808@gmail.com>
2020-11-13 00:11:31 -0300

    definiciones class , definiciones.js

---
+++ 06-JS-V/homework/definiciones.js
@@ -0,0 +1,3 @@
+prototype: es una forma de agregar funciones(metodos) a nuestras clases.
+Constructors (de Clases): los constructores son funciones que devuelven un objeto con propiedades predeterminadas, que se usa´n
+para crear mas objetos.
